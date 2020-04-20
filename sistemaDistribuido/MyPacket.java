1540e35b8265dc3a95ff01100c9c2bc22050e5d4
marco gallegos <ma_galeza@hotmail.com>
2020-04-20 05:06:55 -0500

    [imp] mypacket
    se agrega estructura y logica base de la actividad relacionada

--- sistemaDistribuido/MyPacket.java
+++ sistemaDistribuido/MyPacket.java
@@ -1,5 +1,4 @@
 package sistemaDistribuido;
-
 /**
  * Clase para abtraer el proceso de ensamblado y desensamblado de los paquetes
  *
@@ -18,5 +17,80 @@ package sistemaDistribuido;
 public class MyPacket {
     public Integer id_emisor, id_receptor;
     public String msg;
+    private Boolean es_cliente;
 
+    public MyPacket(Boolean esCliente){
+
+    }
+
+    public static void printbytes(byte[] arg){
+        System.out.print("bytes : ");
+        for (int i = 0; i < arg.length; i++){
+            System.out.print(String.format("%s",arg[i]));
+        }
+        System.out.println("");
+    }
+
+    public static byte[] shortToBytes(Short numero){
+        byte[] arreglo;
+        String numero_s = String.valueOf(numero);
+        arreglo = numero_s.getBytes();
+        return arreglo;
+    }
+
+    public static short bytesToShort(byte[] arreglo){
+        String numero_s = new String(arreglo);
+        short numero = -1;
+        try{
+            numero = Short.parseShort(numero_s);
+        }catch(NumberFormatException e){
+            numero = -1;
+        }
+        return numero;
+    }
+
+    public static byte[] intToBytes(int numero){
+        byte[] arreglo;
+        String numero_s = String.valueOf(numero);
+        arreglo = numero_s.getBytes();
+        return arreglo;
+    }
+
+    public static int bytesToInt(byte[] arreglo){
+        String numero_s = new String(arreglo);
+        int numero = -1;
+        try{
+            numero = Integer.parseInt(numero_s);
+        }catch(NumberFormatException e){
+            numero = -1;
+        }
+        return numero;
+    }
 }
+
+/**
+ *
+ * este codigo es el de la practica y se necesitara para decodificar y codificar el msg
+
+
+ public static void main(String[] args){
+ short numero = 9;
+ String numero_s = String.valueOf(numero);
+ println(String.format("numero original : " + numero));
+ println("numero str : " + numero_s);
+ byte[] numero_bytes = numero_s.getBytes();
+ printbytes(numero_bytes);
+ String numero_recuperado = new String(numero_bytes);
+ println("numero reuperado str : " + numero_recuperado);
+ short numero2 = -1;
+ try {
+ numero2 = Short.parseShort("10000000");//Short.parseShort(numero_recuperado);
+ }catch(NumberFormatException err){
+ println("valor fuera de rango o incompatible");
+ }catch (Exception err){
+ print(err.toString());
+ }
+ println(String.format("Short Recuperado numero2 : %s", numero2));
+ }
+
+ */
